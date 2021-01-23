9fe5f4576772ea796972144cb03b15fe67280c12
User <123@test.ru>
2021-01-23 15:07:35 +0300

    my_task_5

---
+++ task_5.py
@@ -0,0 +1,15 @@
+proceeds = int(input("Введите значение выручки: "))
+costs = int(input("Введите значение издержек: "))
+if proceeds > costs:
+    profit = proceeds - costs
+    print("Ваша фирма в плюсе, прибыль составляет", profit)
+    profitability = profit // proceeds
+    print("Рентабельность фирмы: ", profitability)
+    workers = int(input("Введите кол-во сотрудников фирмы: "))
+    profit_per_worker = profit / workers
+    print("Прибыль в расчёте на одного сотрудника составляет: ", profit_per_worker)
+elif proceeds == costs:
+    print("Баланс фирмы нулевой")
+else:
+    profit = costs - proceeds
+    print("Ваша фирма в минусе, убыток составляет", profit)
