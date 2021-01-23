7c78411ada44da45f5b5b1534f2d86c67fb65726
User <123@test.ru>
2021-01-23 16:36:01 +0300

    edit task_5

--- task_5.py
+++ task_5.py
@@ -3,8 +3,8 @@ costs = int(input("Введите значение издержек: "))
 if proceeds > costs:
     profit = proceeds - costs
     print("Ваша фирма в плюсе, прибыль составляет", profit)
-    profitability = profit // proceeds
-    print("Рентабельность фирмы: ", profitability)
+    profitability = profit / proceeds
+    print(f"Рентабельность фирмы: {profitability:.2f}")
     workers = int(input("Введите кол-во сотрудников фирмы: "))
     profit_per_worker = profit / workers
     print("Прибыль в расчёте на одного сотрудника составляет: ", profit_per_worker)
