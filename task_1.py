616394f2a8fb143d33a9227b5763e88257323207
User <123@test.ru>
2021-01-23 11:23:04 +0300

    my task_1

---
+++ task_1.py
@@ -0,0 +1,17 @@
+first_var = "hoho"
+print(first_var)
+
+second_var = 123
+print(second_var)
+
+third_var = int(input('Введите любое число: '))
+print("Ваше число: ", third_var)
+
+fourth_var = int(input('Введите ещё одно число: '))
+print("Ваше число: ", fourth_var)
+
+fifth_var = input('Введите слово: ')
+print("Ваше слово: ", fifth_var)
+
+sixth_var = input('Введите слово: ')
+print("Ваше слово: ", fifth_var)
