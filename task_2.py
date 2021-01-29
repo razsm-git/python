82c562d781526c279079d4d491de76ca5bfb147f
User <123@test.ru>
2021-01-29 11:15:20 +0300

    lesson3_02

---
+++ task_2.py
@@ -0,0 +1,3 @@
+def user_info(name,lastname,year,city,email,phone):
+    print(f"Имя: {name},Фамилия: {lastname},Год рождения: {year},Город: {city},E-mail: {email},Телефон: {phone}")
+user_info(name='Ivan',lastname='Ivanov',year=1991,city='Novgorod',email='123@123.ru',phone=889988)
